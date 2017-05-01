3a1820f726ef58e719ee3bc3b7aa853a443699de
Matan Rubin <matan@dronecraft.io>
2017-05-01 19:25:06 +0300

    implement ChoiceOption and MultipleSelectionOption

--- virgene-webapp/src/App.js
+++ virgene-webapp/src/App.js
@@ -6,32 +6,71 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      features: Array(2).fill(null),
-    }
-    this.state.features[0] = {
-      name: "CtrlP",
-      description: "Fuzzy file search",
-      selected: false,
-      visible: true,
-    }
-    this.state.features[1] = {
-      name: "Tagbar",
-      description: "Tag browser",
-      selected: false,
-      visible: true,
-    }
-    this.state.features[2] = {
-      name: "Disable arrow keys",
-      description: "Disable arrow keys, helps you learn to use just hjkl",
-      selected: false,
-      visible: true,
-    }
-    this.state.features[3] = {
-      name: "Highlight search",
-      description: "Highlight search results",
-      selected: false,
-      visible: true,
-    }
+      features: [
+        {
+          "name": "CtrlP",
+          "identifier": "ctrlp",
+          "feature_type": "Plugin",
+          "description": "Full path fuzzy file, buffer, mru, tag, ... finder for Vim.",
+          "enabled": false,
+          "selected": true,
+          "visible": true,
+          "category": "Navigation",
+          "installed": true,
+          "template": "ctrlp.j2",
+          "vundle_installation": "ctrlpvim/ctrlp.vim",
+          "options":
+          [
+            {
+                "name": "CtrlP Command",
+                "identifier": "ctrlp_cmd",
+                "option_type": "Choice",
+                "choices": ["CtrlPMixed", "CtrlPStam", "CtrlPDrek"],
+                "pretty_name": "CtrlP Command",
+                "description": "Which mode should CtrlP start in",
+                "default_value": "CtrlPMixed"
+            },
+            {
+                "name": "CtrlP Extensions",
+                "identifier": "ctrlp_extensions",
+                "option_type": "MultipleSelection",
+                "pretty_name": "CtrlP Extensions",
+                "description": "Select which CtrlP extensions to enable",
+                "default_value": ["dir", "mixed"],
+                "choices": ["tag", "buffertag", "quickfix", "dir", "rtscript", "undo", "line", "changes", "mixed", "bookmarkdir"]
+            },
+            {
+                "name": "CtrlP working path mode",
+                "identifier": "ctrlp_working_path_mode",
+                "option_type": "MultipleSelection",
+                "choices": ["c", "a", "r", "w", "0"],
+                "pretty_name": "CtrlP working path mode",
+                "description": "How should CtrlP choose its working directory when starting?",
+                    "details": "c - the directory of the current file.\na - like \"c\", but only applies when the current working directory outside of CtrlP isn't a direct ancestor of the directory of the current file.\nr - the nearest ancestor that contains one of these directories or files: .git .hg .svn .bzr _darcs\nw - begin finding a root from the current working directory outside of CtrlP instead of from the directory of the current file (default). Only applies when \"r\" is also present.\n 0 or <empty> - disable this feature.",
+                "default_value": "r"
+            }
+          ]
+        },
+        {
+          name: "Tagbar",
+          description: "Tag browser",
+          selected: false,
+          visible: true,
+        },
+        {
+          name: "Disable arrow keys",
+          description: "Disable arrow keys, helps you learn to use just hjkl",
+          selected: false,
+          visible: true,
+        },
+        {
+          name: "Highlight search",
+          description: "Highlight search results",
+          selected: false,
+          visible: true,
+        },
+      ]
+    };
   }
 
   render() {
@@ -67,14 +106,25 @@ class App extends Component {
 }
 
 function FeatureDetails(props) {
+  // TODO FeatureDetails should not calc which feature is selected, it should
+  // be passed to it
   var selectedFeatures = props.features.filter(function(feature) {
     return feature.selected;
   });
+  var feature = selectedFeatures[0];
   if (selectedFeatures.length > 0) {
     return (
       <div className="feature-details">
-        <h1>{selectedFeatures[0].name}</h1>
-        <p>{selectedFeatures[0].description}</p>
+        <h1>{feature.name}</h1>
+        <p>Type: {feature.feature_type}</p>
+        <p>{feature.description}</p>
+        {feature.options.map((option, index) => {
+          switch (option.option_type) {
+            case 'Choice': return (<ChoiceOption option={option}/>);
+            case 'MultipleSelection': return (<MultipleSelectionOption option={option}/>);
+            default: return (<p>{option.name}</p>);
+          }
+        })}
       </div>
     );
   } else {
@@ -83,8 +133,39 @@ function FeatureDetails(props) {
         <h1>Select a feature to show its details...</h1>
       </div>
     );
-
   }
+}
+
+function MultipleSelectionOption(props) {
+  return (
+    <div>
+      <h2>{props.option.name}</h2>
+      <p>{props.option.description}</p>
+        <select multiple>
+          {props.option.choices.map((choice, index) => {
+            return (
+              <option key={index} value={choice}>{choice}</option>
+            );
+          })}
+        </select>
+    </div>
+  );
+}
+
+function ChoiceOption(props) {
+  return (
+    <div>
+      <h2>{props.option.name}</h2>
+      <p>{props.option.description}</p>
+        <select>
+          {props.option.choices.map((choice, index) => {
+            return (
+              <option key={index} value={choice}>{choice}</option>
+            );
+          })}
+        </select>
+    </div>
+  );
 }
 
 class FeaturesNavigator extends React.Component {
