8bdb4dac4487d292346b3d6c3d6b06ca47913715
Matan Rubin <matanaloni@gmail.com>
2017-07-22 01:26:25 +0300

    Restructure frontend project, add webpack, hot reloading, sass, eslint
    Followed this great tutorial:
    http://kloc.io/setting-up-react-workflow-babel-webpack/
    
    Did not complete the setup for production environment.

---
+++ frontend/webpack.config.babel.js
@@ -0,0 +1,56 @@
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import {HotModuleReplacementPlugin} from 'webpack';
+
+const defaultEnv = {
+    dev: true,
+    production: false,
+};
+
+export default (env = defaultEnv) => ({
+  entry: [
+    'react-hot-loader/patch', // Needed to preserve state
+    'webpack-dev-server/client?http://localhost:8080', // webpack dev server host and port
+    path.join(__dirname, 'src/index.jsx'),
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+        filename: 'index.html',
+        template: './src/index.html'
+    }),
+    new HotModuleReplacementPlugin(), // Globally enable hot code replacement
+  ],
+  devServer: {
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /.jsx?$/,
+        exclude: /node_modules/,
+        include: path.join(__dirname, 'src'),
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              babelrc: false,
+              plugins: ['react-hot-loader/babel'],
+              presets: [
+                ['es2015', { modules: false }],
+                'react',
+              ],
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(css|scss|sass)$/,
+        loader: 'style-loader!css-loader!sass-loader',
+      },
+    ]
+  },
+});
